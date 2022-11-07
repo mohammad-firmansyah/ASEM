5bf481a3218c8d61a82277851e5849a7a1d69b7b
[Mohammad Firmansya] <[penamas842@gmail.com]>
2022-11-07 16:52:41 +0700

    add currency editText

--- app/src/main/java/com/example/asem/MainActivity.java
+++ app/src/main/java/com/example/asem/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.asem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner inp_tipe_asset;
@@ -11,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
