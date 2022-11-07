a893cc9b189f6f98e25477989e3788b50bf9cd47
[Mohammad Firmansya] <[penamas842@gmail.com]>
2022-11-07 09:05:47 +0700

    init project

---
+++ app/src/main/java/com/example/asem/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.asem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner inp_tipe_asset;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
